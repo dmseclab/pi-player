4e5adb785f507ab11462a6c92da9192c47cec4f5
dmseclab <97091994+dmseclab@users.noreply.github.com>
2026-09-14 14:14:20 +0200

    Load website options extension

--- pi_player/application.py
+++ pi_player/application.py
@@ -2,5 +2,6 @@ from __future__ import annotations
 
 from .extended_app import app
 from . import playlist_backup as _playlist_backup  # noqa: F401
+from . import website_options as _website_options  # noqa: F401
 
 __all__ = ["app"]
