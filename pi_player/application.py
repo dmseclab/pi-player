9c34cd1b2c9e6872698162d38c89e0eff10afcd6
dmseclab <97091994+dmseclab@users.noreply.github.com>
2026-09-15 09:40:07 +0200

    Load appliance management modules

--- pi_player/application.py
+++ pi_player/application.py
@@ -3,5 +3,7 @@ from __future__ import annotations
 from .extended_app import app
 from . import playlist_backup as _playlist_backup  # noqa: F401
 from . import website_options as _website_options  # noqa: F401
+from . import appliance as _appliance  # noqa: F401
+from . import player_info as _player_info  # noqa: F401
 
 __all__ = ["app"]
